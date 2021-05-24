a85d969b74e9ec81e7b0a60d68812df2c9133cbb
scapellas <60827201+scapellas@users.noreply.github.com>
2021-05-24 22:47:46 +0200

    Add files via upload

---
+++ quintic.m
@@ -0,0 +1,40 @@
+function [r] = quintic(orbit)
+
+    % Universal constants
+    c = 299.79e6;
+    G = 6.674e-11;
+    
+    % Sun
+    L = 3.8e26;
+    Ms = 1.989e30;
+    
+    %Earth
+    Me = 5.972e24;
+    R = 1.496e11;
+    T = 365.256*24*3600;
+    omega = 2*pi/T;
+    
+    % Solar sail data
+    A = 1672;
+    m = 90;
+
+    if orbit == 0
+        Meff = Ms;
+    else
+        Meff = Ms - L*A/(2*pi*c*G*m);
+    end
+    
+    alfa = G*m*Meff;
+    beta = G*m*Me;
+    gamma = m*omega^2;
+    
+    a5 = gamma;
+    a4 = -2*R*gamma;
+    a3 = R^2*gamma;
+    a2 = beta - alfa;
+    a1 = 2*R*alfa;
+    a0 = -alfa*R^2;
+    sols = roots([a5, a4, a3, a2, a1, a0]);
+    r = sols(5);
+end
+
---
+++ report.m
@@ -0,0 +1,9 @@
+clear;
+
+% Solution of the quintic
+L1 = quintic(0);
+r = quintic(1);
+
+% Warning time gained thanks to Sub-L1
+v = 400000; % Average solar wind speed
+warning_time_in_minutes = (L1 - r)/(60*v);
