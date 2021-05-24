fa66b354bff85d79250ae6f29b1114dfdf90e155
scapellas <60827201+scapellas@users.noreply.github.com>
2021-05-24 22:48:46 +0200

    Update run.m

--- run.m
+++ run.m
@@ -1,9 +1,9 @@
 clear;
 
-% Solution of the quintic
+% Finds the two equilibrium points.
 L1 = quintic(0);
 r = quintic(1);
 
-% Warning time gained thanks to Sub-L1
-v = 400000; % Average solar wind speed
+% Warning time gained thanks to Sub-L1.
+v = 400000; % Average solar wind speed.
 warning_time_in_minutes = (L1 - r)/(60*v);
