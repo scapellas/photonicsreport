3a353a53c6d5d2cbdcc75fd412816e4842648d1d
scapellas <60827201+scapellas@users.noreply.github.com>
2021-05-24 22:58:38 +0200

    Update main.m

--- main.m
+++ main.m
@@ -3,5 +3,5 @@ rL = quintic(0);
 rS = quintic(1);
 
 % Warning time gained thanks to Sub-L1.
-v = 400000; % Average solar wind speed.
+v = 400000; % Average speed of solar wind in m/s.
 warning_time_in_minutes = (rL - rS)/(60*v);
